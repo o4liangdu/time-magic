cd5fa92df95f2d8f7bc9d7e8612f18bf5ac085a8
liangdo <liangdo18@qq.com>
2021-08-22 18:00:10 +0800

    sub change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ timeStr }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +32,20 @@
 </template>
 
 <script>
-export default {
+const { formatTime } = require('../../sub/time-magic/index');
+console.log(formatTime, "yyy")
+export default {  
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      timeStr: formatTime(new Date(), "{y}-{m}-{d} {h}:{i}")
+    }
+  },
+  mounted() {
+    
   }
 }
 </script>
